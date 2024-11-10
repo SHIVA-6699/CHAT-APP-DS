84b8ef3b79c77e089d21c483ec936a2a19a6bcdb
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-11-10 11:10:30 +0530

    [RE COMMIT]

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
@@ -8,6 +9,8 @@ import userRoutes from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 dotenv.config();
 
 
@@ -17,6 +20,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get("/", (req, res) => {
 //   res.send("Hello World");
 // });
